6614b13438de4d8ee11b150de21dd6f2ec3b511d
AidenQuirindongo <aidenquirindongo@gmail.com>
2026-06-16 01:07:52 -0400

    Project 2
    modified code of project 1 for project 2

--- Policy.java
+++ Policy.java
@@ -1,7 +1,9 @@
 
+/**
+   The Policy class stores insurance policy information.
+*/
+
 public class Policy
-
-
 {
 private String policyNumber;
 private String providerName;
@@ -12,6 +14,7 @@ private String smokingStatus;
 private double height;
 private double weight;
 
+  
 public Policy()
 {
 policyNumber = "";
@@ -19,16 +22,17 @@ providerName = "";
 firstName = "";
 lastName = "";
 age = 0;
-smokingStatus = "non-smoker";
+smokingStatus = "";
 height = 0.0;
 weight = 0.0;
 }
 
-public Policy(String pNumber, String provider, String fName, String lName, int holderAge, String smokerStatus, double holderHeight, double holderWeight)
-
+  
+public Policy(String policyNum, String provider, String fName,
+String lName, int holderAge, String smokerStatus,
+double holderHeight, double holderWeight)
 {
-
-policyNumber = pNumber;
+policyNumber = policyNum;
 providerName = provider;
 firstName = fName;
 lastName = lName;
@@ -38,98 +42,135 @@ height = holderHeight;
 weight = holderWeight;
 }
 
-public void setPolicyNumber(String pNumber)
+ 
+public void setPolicyNumber(String policyNum)
 {
-policyNumber = pNumber;
+policyNumber = policyNum;
 }
 
-public String getPolicyNumber()
-{
-return policyNumber;
-}
+  
 public void setProviderName(String provider)
 {
 providerName = provider;
 }
-public String getProviderName()
-{
-return providerName;
-}
+
+ 
 public void setFirstName(String fName)
 {
 firstName = fName;
 }
-public String getFirstName()
-{
-return firstName;
-}
+
+   
 public void setLastName(String lName)
 {
 lastName = lName;
 }
-public String getLastName()
-{
-return lastName;
-}
+
+   
 public void setAge(int holderAge)
 {
 age = holderAge;
 }
-public int getAge()
-{
-return age;
-}
+
+   
 public void setSmokingStatus(String smokerStatus)
 {
 smokingStatus = smokerStatus;
 }
-public String getSmokingStatus()
-{
-return smokingStatus;
-}
+
+  
 public void setHeight(double holderHeight)
 {
 height = holderHeight;
 }
-public double getHeight()
-{
-return height;
-}
+
+ 
 public void setWeight(double holderWeight)
 {
 weight = holderWeight;
 }
+
+   
+public String getPolicyNumber()
+{
+return policyNumber;
+}
+
+  
+public String getProviderName()
+{
+return providerName;
+}
+
+  
+public String getFirstName()
+{
+return firstName;
+}
+
+   
+public String getLastName()
+{
+return lastName;
+}
+
+   
+public int getAge()
+{
+return age;
+}
+
+  
+public String getSmokingStatus()
+{
+return smokingStatus;
+}
+
+  
+public double getHeight()
+{
+return height;
+}
+
+   
 public double getWeight()
 {
 return weight;
 }
 
+  
 public double getBMI()
 {
 return (weight * 703) / (height * height);
 }
 
+   
 public double getPolicyPrice()
 {
-double price = 600.0;
+final double BASE_FEE = 600.0;
+final double AGE_FEE = 75.0;
+final double SMOKER_FEE = 100.0;
+final double BMI_FEE_RATE = 20.0;
+final double BMI_LIMIT = 35.0;
+
+double price = BASE_FEE;
 double bmi = getBMI();
 
 if (age > 50)
 {
-price = price + 75.0;
-}
-if (smokingStatus.equalsIgnoreCase("smoker"))
-{
-price = price + 100.0;
+price += AGE_FEE;
 }
 
-if (bmi > 35)
+if (smokingStatus.equalsIgnoreCase("smoker"))
 {
-price = price + ((bmi - 35) * 20);
+price += SMOKER_FEE;
+}
+
+if (bmi > BMI_LIMIT)
+{
+price += (bmi - BMI_LIMIT) * BMI_FEE_RATE;
 }
 
 return price;
-
 }
-
 }
